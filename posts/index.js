d09eb6dabb7055103ae7d5926ac04d5cc7a0e07c
josdugan <josdugan@gmail.com>
2021-11-03 22:38:08 -0400

    posts service initial commit

---
+++ posts/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 4000;
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    posts[id] = {
+        id, title
+    };
+
+    res.status(201).send(posts[id]);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+});
